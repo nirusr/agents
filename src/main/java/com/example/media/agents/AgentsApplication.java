af21cb910464e347a52e535796ef882d524f0aa3
Srinivasan Govindaraji <srgovin2@cisco.com>
2017-12-23 08:37:31 -0800

    initial commit

---
+++ src/main/java/com/example/media/agents/AgentsApplication.java
@@ -0,0 +1,12 @@
+package com.example.media.agents;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgentsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgentsApplication.class, args);
+	}
+}
